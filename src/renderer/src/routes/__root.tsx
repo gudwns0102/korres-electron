ffe8279dc1f814baf7989e7ee5868079fe036c98
김형준 <hj@team-mint.io>
2024-09-10 14:54:00 +0900

    update

--- src/renderer/src/routes/__root.tsx
+++ src/renderer/src/routes/__root.tsx
@@ -1,7 +1,7 @@
 import { createRootRoute, Outlet } from "@tanstack/react-router";
 
 import { AppContext } from "@renderer/App";
-import { Button, Form, FormProps, Input } from "antd";
+import { Button, Form, FormProps, Input, Typography } from "antd";
 import { useContext } from "react";
 import { useSession } from "../hooks/useSession";
 
@@ -25,8 +25,11 @@ function LoginPage() {
 
   const onFinish: FormProps<FieldType>["onFinish"] = async (values) => {
     if (values.id && values.password) {
-      console.log(await session.login(values.id, values.password));
-      console.log(session.cookieJar.toJSON());
+      try {
+        await session.login(values.id, values.password);
+      } catch (error) {
+        window.alert(error);
+      }
     }
   };
 
@@ -35,24 +38,39 @@ function LoginPage() {
   };
 
   return (
-    <Form onFinish={onFinish} onFinishFailed={onFinishFailed}>
-      <Form.Item<FieldType>
-        name="id"
-        rules={[{ required: true, message: "코레일 회원번호를 입력해 주세요" }]}
+    <div
+      style={{
+        margin: 16,
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "100vh"
+      }}
+    >
+      <Form
+        style={{ width: "100%", maxWidth: 300 }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
       >
-        <Input autoFocus placeholder="코레일 회원번호" defaultValue={""} />
-      </Form.Item>
-      <Form.Item<FieldType>
-        name="password"
-        rules={[{ required: true, message: "코레일 회원번호를 입력해 주세요" }]}
-      >
-        <Input type="password" placeholder="비밀번호" defaultValue={""} />
-      </Form.Item>
-      <Form.Item>
-        <Button type="primary" htmlType="submit" className="login-form-button">
-          Submit
-        </Button>
-      </Form.Item>
-    </Form>
+        <Typography.Title level={3}>Korres</Typography.Title>
+        <Form.Item<FieldType>
+          name="id"
+          rules={[{ required: true, message: "코레일 회원번호를 입력해 주세요" }]}
+        >
+          <Input autoFocus placeholder="코레일 회원번호" defaultValue={""} />
+        </Form.Item>
+        <Form.Item<FieldType>
+          name="password"
+          rules={[{ required: true, message: "코레일 회원번호를 입력해 주세요" }]}
+        >
+          <Input type="password" placeholder="비밀번호" defaultValue={""} />
+        </Form.Item>
+        <Form.Item>
+          <Button type="primary" htmlType="submit" className="login-form-button">
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
   );
 }
